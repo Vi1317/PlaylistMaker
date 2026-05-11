5ca1c6cac5bbb3e5856cf1dcfb0ed37ea16f6e2a
Vi1317 <leejiah1318@gmail.com>
2026-05-11 21:15:56 +0700

    20 спринт

--- app/src/main/java/com/example/playlistmaker/search/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/playlistmaker/search/data/network/RetrofitNetworkClient.kt
@@ -3,8 +3,7 @@ package com.example.playlistmaker.search.data.network
 import com.example.playlistmaker.search.data.dto.TrackSearchRequest
 import com.example.playlistmaker.search.data.NetworkClient
 import com.example.playlistmaker.search.data.dto.Response
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlin.coroutines.cancellation.CancellationException
 
 class RetrofitNetworkClient(private val iTunesService: ITunesApi) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
@@ -12,13 +11,13 @@ class RetrofitNetworkClient(private val iTunesService: ITunesApi) : NetworkClien
             return Response().apply { resultCode = 400 }
         }
 
-        return withContext(Dispatchers.IO) {
-            try {
-                val response = iTunesService.search(dto.expression)
-                response.apply { resultCode = 200 }
-            } catch (e: Throwable) {
-                Response().apply { resultCode = 500 }
-            }
+        return try {
+            val response = iTunesService.search(dto.expression)
+            response.apply { resultCode = 200 }
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            Response().apply { resultCode = 500 }
         }
     }
 }
